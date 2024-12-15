443c99f9d52c27bef53fe88135b615cbc4cb91ef
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2024-12-15 12:37:57 +0530

    after redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Enables strict mode for React
+    swcMinify: true, // Enables SWC compiler for faster builds
+    trailingSlash: false, // Set to true if you want URLs to have trailing slashes
+  };
+  
+  export default nextConfig;
+  
