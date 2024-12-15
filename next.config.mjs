52150a6f060e87f37234359969b4c4767f5fe2dc
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2024-12-15 14:22:59 +0530

    after next

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 const nextConfig = {
-    reactStrictMode: true, // Enables strict mode for React
-    swcMinify: true, // Enables SWC compiler for faster builds
-    trailingSlash: false, // Set to true if you want URLs to have trailing slashes
+    reactStrictMode: true,
   };
   
   export default nextConfig;
